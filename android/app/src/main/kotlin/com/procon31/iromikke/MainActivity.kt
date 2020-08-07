fc90161e1efef23cdf5f6d823faf478512ec56d6
towa0131 <towa.towa0131@gmail.com>
2020-08-07 22:14:34 +0900

    [Add] 最初のコミット

---
+++ android/app/src/main/kotlin/com/procon31/iromikke/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.procon31.iromikke
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
