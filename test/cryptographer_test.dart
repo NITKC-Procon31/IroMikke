e4afd9cfd991b2f43c4523ae9c7536bf51ed4bb0
towa0131 <towa.towa0131@gmail.com>
2020-09-28 00:54:46 +0900

    [Add] Cryptographerのテストを追加

---
+++ test/cryptographer_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:iromikke/network/api/cryptographer.dart';
+
+void main(){
+  test('Cryptographer test', (){
+    String str = 'Hello, World!';
+    String encoded = Cryptographer.encode(str);
+    String decoded = Cryptographer.decode(encoded);
+
+    print("Test String : $str");
+    print("Encode : $encoded");
+    print("Decode : $decoded");
+    expect(str, decoded);
+  });
+
+}
